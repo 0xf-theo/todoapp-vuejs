65c3cf874254199df03eb0d5ec23169befa14ec2
Noa Cantrelle <95468346+NoooC@users.noreply.github.com>
2023-10-30 17:26:36 +0100

    Update index.js

--- backend/src/api/routes/index.js
+++ backend/src/api/routes/index.js
@@ -1,5 +1,10 @@
 "use strict";
 
+//Ce fichier agit comme un point d'entrée pour les routes de votre application. 
+//Il crée un routeur Express et utilise router.use() pour monter les routes définies dans task.route.js sous le chemin /task. 
+//Cela signifie que toutes les routes définies dans task.route.js seront préfixées par /task lorsque votre application Express sera montée. 
+//Par exemple, si vous avez une route GET / dans task.route.js, elle sera accessible en tant que GET /task/ lorsque vous utiliserez index.js comme point d'entrée.
+
 const express = require("express");
 const router = express.Router();
 
