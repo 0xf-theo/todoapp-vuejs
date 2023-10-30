fe6dab7a45f53cb33d0291e439435ec2ef7f218f
Noa Cantrelle <95468346+NoooC@users.noreply.github.com>
2023-10-30 17:29:43 +0100

    Update index.js

--- backend/src/database/models/index.js
+++ backend/src/database/models/index.js
@@ -1,3 +1,8 @@
+
+//ce fichier agit comme un module d'exportation qui permet d'importer les modèles Mongoose associés aux tâches 
+//et aux utilisateurs dans d'autres parties de votre application. 
+//Cela simplifie la gestion des opérations de base de données et la réutilisation de ces modèles à travers votre application
+
 const TaskModel = require("./task.model");
 const UserModel = require("./user.model");
 
