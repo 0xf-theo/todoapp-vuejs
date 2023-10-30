0c2fed098c232305d470e69d8262c06d2973f76c
Noa Cantrelle <95468346+NoooC@users.noreply.github.com>
2023-10-30 17:27:31 +0100

    Update task.schema.js

--- backend/src/utils/schemas/task.schema.js
+++ backend/src/utils/schemas/task.schema.js
@@ -1,5 +1,10 @@
 "use strict";
 
+//this code is to define a schema for validating task objects. 
+//We use this schema to ensure that task data provided to your application meets specific criteria, 
+//such as data types, lengths, and date constraints. This is particularly useful for input validation, 
+//data integrity, and ensuring that your application processes data that adheres to a predefined structure
+
 const joi = require("joi");
 
 const taskSchema = joi.object({
