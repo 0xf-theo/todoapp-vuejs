def69d4abc025e8ecca9e83e72c441dc59e4fa98
Noa Cantrelle <95468346+NoooC@users.noreply.github.com>
2023-10-30 17:37:34 +0100

    Update task.controller.js

--- backend/src/api/controllers/task.controller.js
+++ backend/src/api/controllers/task.controller.js
@@ -1,8 +1,17 @@
 "use strict";
 
+//Ce code est un ensemble de fonctions qui gèrent les opérations CRUD (Create, Read, Update, Delete) 
+//pour les tâches (tasks) dans une application
+
 const validator = require("../../utils/helpers/validator");
 const { TaskModel } = require("../../database/models");
 const taskSchema = require("../../utils/schemas/task.schema");
+
+
+//Cette fonction gère la récupération des tâches. Elle prend en compte les filtres tels que la priorité et le statut 
+//pour renvoyer une liste de tâches correspondant à ces critères. 
+//Les tâches sont filtrées en fonction de l'utilisateur actuellement connecté, ce qui signifie qu'un utilisateur ne peut voir 
+//que ses propres tâches. Les résultats sont renvoyés au format JSON avec un message de succès.
 
 const listing = async (req, res, next) => {
   try {
@@ -30,6 +39,11 @@ const listing = async (req, res, next) => {
   }
 };
 
+//Cette fonction récupère les détails d'une tâche spécifique en fonction de son identifiant (ID). 
+//Elle vérifie également si l'utilisateur actuellement connecté est le propriétaire de cette tâche. 
+//Si ce n'est pas le cas, une erreur est générée indiquant que l'utilisateur n'est pas autorisé à accéder à cette tâche. 
+//Les détails de la tâche sont renvoyés au format JSON en cas de succès.
+
 const detail = async (req, res, next) => {
   try {
     const id = req.params.id;
@@ -50,6 +64,10 @@ const detail = async (req, res, next) => {
   }
 };
 
+//Cette fonction crée une nouvelle tâche en utilisant les données fournies dans la requête. 
+//Elle associe également la tâche à l'utilisateur actuellement connecté en tant que créateur. 
+//Avant de créer la tâche, elle valide les données en utilisant un schéma de validation spécifique (taskSchema).
+
 const create = async (req, res, next) => {
   try {
     req.body.createdBy = req?.custom?._id;
@@ -67,6 +85,10 @@ const create = async (req, res, next) => {
     next(err);
   }
 };
+
+//Cette fonction met à jour les détails d'une tâche existante en fonction de son identifiant. 
+//Elle vérifie également si l'utilisateur actuellement connecté est le propriétaire de la tâche avant de permettre la mise à jour. 
+//Les détails de la tâche (titre, description, date d'échéance, priorité, sous-tâches) sont mis à jour avec les nouvelles données de la requête. 
 
 const update = async (req, res, next) => {
   try {
@@ -98,6 +120,10 @@ const update = async (req, res, next) => {
     next(err);
   }
 };
+
+//Cette fonction supprime une tâche en fonction de son identifiant. 
+//Elle vérifie si l'utilisateur actuellement connecté est le propriétaire de la tâche avant de permettre la suppression. 
+//En cas de succès, elle renvoie un message indiquant que la tâche a été supprimée.
 
 const destroy = async (req, res, next) => {
   try {
