b1194ebd4aca4916486cc46c7472b2f977beb804
Noa Cantrelle <95468346+NoooC@users.noreply.github.com>
2023-10-30 17:25:48 +0100

    Update task.route.js

--- backend/src/api/routes/task.route.js
+++ backend/src/api/routes/task.route.js
@@ -1,5 +1,10 @@
 "use strict";
 
+//Ce fichier définit les routes spécifiques liées à la gestion des tâches. 
+//Il utilise Express pour créer un routeur (router) et associe des chemins d'URL spécifiques à des fonctions de contrôleur 
+//(comme taskController.listing, taskController.detail, etc.). 
+//Ces fonctions de contrôleur sont responsables de la logique métier associée à chaque route. 
+
 const express = require("express");
 const taskController = require("../controllers/task.controller");
 
