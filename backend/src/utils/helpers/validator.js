bca417ee196274a7f57053901ec7d1ab16340afd
Noa Cantrelle <95468346+NoooC@users.noreply.github.com>
2023-10-30 17:29:06 +0100

    Update validator.js

--- backend/src/utils/helpers/validator.js
+++ backend/src/utils/helpers/validator.js
@@ -1,5 +1,10 @@
 "use strict";
 
+//ce module offre une fonction de validation qui peut être utilisée pour valider des données par rapport à un schéma Joi. 
+//Si les données ne correspondent pas aux règles du schéma, une erreur est générée avec un message d'erreur indiquant la 
+//première erreur de validation rencontrée. Cela peut être utile pour garantir que les données entrantes dans votre application 
+//sont conformes à des règles spécifiques définies par le schéma.
+
 const validateSchema = (schema, data) => {
   const result = schema.validate(data, {
     errors: { wrap: { label: false } },
