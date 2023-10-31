2cad9aa91c62431def9a3d12e927ea35ebb1ba98
Théo <villano.theo@hotmail.fr>
2023-10-31 09:37:02 +0100

    Update protect.js

--- backend/src/api/middleware/protect.js
+++ backend/src/api/middleware/protect.js
@@ -1,19 +1,29 @@
+// Importation des dépendances
+
 const { promisify } = require("util");
 const jwt = require("jsonwebtoken");
 const { UserModel } = require("../../database/models");
 
+// Middleware d'authentification utilisateur
 module.exports = async (req, res, next) => {
   let token;
+
+  // Vérifie si un jeton JWT est inclus dans la requête
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith("Bearer")
   ) {
+
+    // Extrait le jeton des en-têtes de la requête
+    // (au format "Bearer <token>")
     // eslint-disable-next-line prefer-destructuring
     token = req.headers.authorization.split(" ")[1];
   } else if (req.cookies && req.cookies.jwt) {
+    // Extrait le jeton des cookies de la requête
     token = req.cookies.jwt;
   }
 
+  // Si aucun jeton n'est trouvé, renvoie une réponse d'erreur 401
   if (!token) {
     return res.status(401).send({
       error: true,
@@ -26,8 +36,11 @@ module.exports = async (req, res, next) => {
     "MY_SECRET_KEY"
   );
 
+  // Recherche l'utilisateur correspondant dans la base de données
   const user = await UserModel.findById(decoded.id).exec();
 
+  // En cas d'erreur lors de la vérification du jeton, ou si l'utilisateur n'est pas trouvé
+    // dans la base de données, renvoie une réponse d'erreur 401
   if (!user) {
     return res.status(401).send({
       error: true,
